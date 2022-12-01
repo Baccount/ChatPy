337f669068a9bd0da49d34f9e9faee9444a47eac
“Brandon <cassias.long0r@icloud.com>
2022-11-30 17:41:35 -0800

    fixes

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 """
 
 Usage:
@@ -17,95 +15,62 @@ bot.
 
 import logging
 
-
 from telegram import __version__ as TG_VER
 
-
 try:
-
     from telegram import __version_info__
-
 except ImportError:
-
     __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
 
 
 if __version_info__ < (20, 0, 0, "alpha", 5):
-
     raise RuntimeError(
-
         f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-
         f"{TG_VER} version of this example, "
-
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-
     )
 import os
+
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-
 from telegram.ext import (
-
     Application,
-
     CommandHandler,
-
     ContextTypes,
-
     ConversationHandler,
-
     MessageHandler,
-
     filters,
-
 )
-
 
 # Enable logging
 
 logging.basicConfig(
-
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-
 )
-
 logger = logging.getLogger(__name__)
-
 
 GENDER, PHOTO, LOCATION, BIO = range(4)
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Starts the conversation and asks the user about their gender."""
 
     reply_keyboard = [["Boy", "Girl", "Other"]]
 
-
     await update.message.reply_text(
-
         "Hi! My name is Professor Bot. I will hold a conversation with you. "
-
         "Send /cancel to stop talking to me.\n\n"
-
         "Are you a boy or a girl?",
-
         reply_markup=ReplyKeyboardMarkup(
-
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-
+            reply_keyboard,
+            one_time_keyboard=True,
+            input_field_placeholder="Boy or Girl?",
         ),
-
     )
-
 
     return GENDER
 
 
-
 async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Stores the selected gender and asks for a photo."""
 
     user = update.message.from_user
@@ -113,64 +78,40 @@ async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     logger.info("Gender of %s: %s", user.first_name, update.message.text)
 
     await update.message.reply_text(
-
         "I see! Please send me a photo of yourself, "
-
         "so I know what you look like, or send /skip if you don't want to.",
-
         reply_markup=ReplyKeyboardRemove(),
-
     )
-
-
     return PHOTO
 
 
-
 async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Stores the photo and asks for a location."""
 
     user = update.message.from_user
-
     photo_file = await update.message.photo[-1].get_file()
 
     await photo_file.download_to_drive("user_photo.jpg")
-
     logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
 
     await update.message.reply_text(
-
         "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
-
     )
-
-
     return LOCATION
-
 
 
 async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Skips the photo and asks for a location."""
 
     user = update.message.from_user
-
     logger.info("User %s did not send a photo.", user.first_name)
-
     await update.message.reply_text(
-
         "I bet you look great! Now, send me your location please, or send /skip."
-
     )
-
-
     return LOCATION
 
 
-
 async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Stores the location and asks for some info about the user."""
 
     user = update.message.from_user
@@ -178,24 +119,20 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_location = update.message.location
 
     logger.info(
-
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-
+        "Location of %s: %f / %f",
+        user.first_name,
+        user_location.latitude,
+        user_location.longitude,
     )
 
     await update.message.reply_text(
-
         "Maybe I can visit you sometime! At last, tell me something about yourself."
-
     )
-
 
     return BIO
 
 
-
 async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Skips the location and asks for info about the user."""
 
     user = update.message.from_user
@@ -203,29 +140,21 @@ async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     logger.info("User %s did not send a location.", user.first_name)
 
     await update.message.reply_text(
-
         "You seem a bit paranoid! At last, tell me something about yourself."
-
     )
-
 
     return BIO
 
 
-
 async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
     """Stores the info about the user and ends the conversation."""
 
     user = update.message.from_user
-
     logger.info("Bio of %s: %s", user.first_name, update.message.text)
 
     await update.message.reply_text("Thank you! I hope we can talk again some day.")
 
-
     return ConversationHandler.END
-
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -237,14 +166,13 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     logger.info("User %s canceled the conversation.", user.first_name)
 
     await update.message.reply_text(
-
         "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-
     )
 
+    return  # This is a comment.
 
-    return ConversationHandler.END
 
+conversation_handler = ConversationHandler().END
 
 
 def main() -> None:
@@ -255,43 +183,30 @@ def main() -> None:
     print(os.environ.get("TOKEN"))
     application = Application.builder().token(os.environ.get("TOKEN")).build()
 
-
     # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
 
     conv_handler = ConversationHandler(
-
         entry_points=[CommandHandler("start", start)],
-
         states={
-
             GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
-
-            PHOTO: [MessageHandler(filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
-
-            LOCATION: [
-
-                MessageHandler(filters.LOCATION, location),
-
-                CommandHandler("skip", skip_location),
-
+            PHOTO: [
+                MessageHandler(filters.PHOTO, photo),
+                CommandHandler("skip", skip_photo),
             ],
-
+            LOCATION: [
+                MessageHandler(filters.LOCATION, location),
+                CommandHandler("skip", skip_location),
+            ],
             BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
-
         },
-
         fallbacks=[CommandHandler("cancel", cancel)],
-
     )
 
-
     application.add_handler(conv_handler)
-
 
     # Run the bot until the user presses Ctrl-C
 
     application.run_polling()
-
 
 
 if __name__ == "__main__":
